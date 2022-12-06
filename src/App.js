6460e2655d1359b61ca6c870adf90df7f570a810
Oussama Fajraoui <102801929+Oussama-Fajraoui@users.noreply.github.com>
2022-12-06 13:49:39 +0100

    set pages according to the Figma picture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import "./App.scss";
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
+import Contact from "./pages/contact/Contact";
+
+
+
 
 function App() {
+
+  // const [first, setFirst] = useState("Zi");
+
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <>
+     <BrowserRouter>
+        <Header />
+           <Routes>
+            <Route path="/" element={ <Home />} />
+            <Route path="/contact" element={ <Contact />} />
+           </Routes>
+        <Footer />
+     </BrowserRouter>
+    </>
   );
 }
 
