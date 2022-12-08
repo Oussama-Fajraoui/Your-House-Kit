5360c6c47ded3658f9be6fdd972107197714718d
Oussama Fajraoui <102801929+Oussama-Fajraoui@users.noreply.github.com>
2022-12-08 13:44:05 +0100

    set the loader photo

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -22,5 +22,5 @@ export const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getFirestore(app);
+export const storage = getStorage(app);
 export default app;
