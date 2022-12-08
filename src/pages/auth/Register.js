33b5ee7162cd0718bfbbf1e3d825414827181c0d
Oussama Fajraoui <102801929+Oussama-Fajraoui@users.noreply.github.com>
2022-12-08 12:02:50 +0100

    register

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import styles from "./auth.module.scss";
+import registerImg from "../../assests/register.png";
+import Card from "../../components/card/Card"; 
+import { Link } from 'react-router-dom';
+import { FaGoogle } from 'react-icons/fa';
+
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <section className={`conatiner ${styles.auth}`}>
+   
+        <Card>
+        <div className={styles.form}>
+            <h2>Register</h2>
+            
+            <form>
+                <input type="text" placeholder='Email' required />
+                <input type="password" placeholder='Password' required />
+                <input type="password" placeholder='Confirm Password' required />
+                <button className='--btn --btn-primary --btn-block'>Register</button>
+
+            </form>
+            <span className={styles.register}>
+                <p>Already have an account ?</p>
+                <Link to="/login">Login</Link>
+            </span>
+        </div>
+        </Card>
+        <div className={styles.img}>
+            <img src={registerImg} alt='Register' width="400" />
+        </div>
+    </section>
   )
 }
 
